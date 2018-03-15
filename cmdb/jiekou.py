fa04754ade23e55702ea7413033089a5948eae7b
linuxvip <525277891@qq.com>
2018-03-15 13:53:38 +0800

    api

--- cmdb/jiekou.py
+++ cmdb/jiekou.py
@@ -41,3 +41,6 @@ class IdcViewSet(viewsets.ModelViewSet):
 class HostViewSet(viewsets.ModelViewSet):
     queryset = Host.objects.all()
     serializer_class = HostSerializer
+    permission_classes = (permissions.IsAuthenticated,) # 权限检查，支持7种权限
+    filter_backends = (filters.SearchFilter,)           # rest-framework 提供的默认的filters
+    search_fields = ('hostname', 'ip')                     # 指定搜索的域,http://example.com/api/idc/?search=Azure
