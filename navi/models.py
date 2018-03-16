004a3a4d8f3d1056f2098254959c4fd5d49bf45d
linuxvip <525277891@qq.com>
2018-03-16 15:01:31 +0800

    add navi field momo

--- navi/models.py
+++ navi/models.py
@@ -10,6 +10,7 @@ class navi(models.Model):
     name = models.CharField(u"名称",max_length=50)
     description = models.CharField(u"描述",max_length=50)
     url = models.URLField(u"网址")
+    memo = models.TextField(u"备注信息", max_length=200, null=True, blank=True)
 
     def __unicode__(self):
         return self.name
